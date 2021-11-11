ef0aa46b9add0b51ccb188a36fb4147cb9e8af4a
Drycargo <1007374120@qq.com>
2021-11-11 15:37:06 -0800

    Class renamed

--- Minesweeper/src/MS/test.java
+++ Minesweeper/src/MS/test.java
@@ -7,7 +7,7 @@ public class test {
     public static void main(String[] args) {
         EventQueue.invokeLater(() ->
         {
-            JFrame f1 = new ms();
+            JFrame f1 = new MineSweeperGame();
             f1.setTitle("Mine_sweeper");
             f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             f1.setVisible(true);
